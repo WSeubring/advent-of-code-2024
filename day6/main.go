2ee4e745b1c6bc2beb4521a83af5e4f50a57072c
Wesley Seubring <w.seubring@gmail.com>
2024-12-10 13:05:37 +0100

    day 6: part one

--- day6/main.go
+++ day6/main.go
@@ -22,11 +22,11 @@ type Direction struct {
 
 type GuardPatrol struct {
 	Map   Map
-	guard Guard
+	Guard Guard
 }
 
-func (g *GuardPatrol) GetGuard() Guard {
-	for y, row := range g.Map {
+func (m Map) GetGuard() Guard {
+	for y, row := range m {
 		for x, cell := range row {
 			if cell == '^' {
 				return Guard{
@@ -45,12 +45,87 @@ type Guard struct {
 	Direction Direction
 }
 
+func (g *Guard) TurnRight() {
+	newX := -g.Direction.Y
+	newY := g.Direction.X
+	g.Direction.X = newX
+	g.Direction.Y = newY
+}
+
+func (g *Guard) MoveForward() {
+	g.Position.X += g.Direction.X
+	g.Position.Y += g.Direction.Y
+}
+
+func (g *Guard) GetNextLocation() Position {
+	nextPosition := Position{
+		X: g.Position.X + g.Direction.X,
+		Y: g.Position.Y + g.Direction.Y,
+	}
+
+	return nextPosition
+}
+
+func (m Map) IsObstacle(position Position) bool {
+	return m[position.Y][position.X] == '#'
+}
+
+func (m Map) IsInBounds(position Position) bool {
+	return position.X >= 0 && position.Y >= 0 && position.Y < len(m) && position.X < len(m[position.Y])
+}
+
+func (m Map) Mark(position Position) {
+	m[position.Y][position.X] = 'X'
+}
+
+func (gp GuardPatrol) MarkPatrol() {
+	gp.Map.Mark(gp.Guard.Position)
+
+	for {
+		nextPosition := gp.Guard.GetNextLocation()
+
+		if !gp.Map.IsInBounds(nextPosition) {
+			break
+		}
+
+		if !gp.Map.IsObstacle(nextPosition) {
+			gp.Guard.MoveForward()
+		} else if gp.Map.IsObstacle(nextPosition) {
+			gp.Guard.TurnRight()
+		}
+
+		gp.Map.Mark(gp.Guard.Position)
+	}
+}
+
+func (m Map) CountMarked() int {
+	count := 0
+	for _, row := range m {
+		for _, cell := range row {
+			if cell == 'X' {
+				count++
+			}
+		}
+	}
+	return count
+}
+
 //go:embed input.txt
 var input string
 
 func SolveA() {
 	patrolMap := ParseMap(input)
-	fmt.Print(utils.RunesToString(patrolMap))
+	guard := patrolMap.GetGuard()
+
+	patrol := GuardPatrol{
+		Map:   patrolMap,
+		Guard: guard,
+	}
+
+	patrol.MarkPatrol()
+
+	fmt.Println(utils.RunesToString(patrol.Map))
+	fmt.Println(patrol.Map.CountMarked())
 }
 
 // Parsing
--- day6/main_test.go
+++ day6/main_test.go
@@ -11,16 +11,44 @@ func TestSolveA(t *testing.T) {
 }
 
 func TestGetGuard(t *testing.T) {
-	g := GuardPatrol{
-		Map: [][]rune{
-			[]rune("  ^  "),
-			[]rune("     "),
-			[]rune("     "),
-		},
-	}
-	guard := g.GetGuard()
-	assert.Equal(t, 2, guard.Position.X)
-	assert.Equal(t, 0, guard.Position.Y)
+	m := Map([][]rune{
+		[]rune("   "),
+		[]rune(" ^ "),
+		[]rune("   "),
+	})
+	guard := m.GetGuard()
+	assert.Equal(t, 1, guard.Position.X)
+	assert.Equal(t, 1, guard.Position.Y)
 	assert.Equal(t, 0, guard.Direction.X)
 	assert.Equal(t, -1, guard.Direction.Y)
+}
+
+func TestTurnRight(t *testing.T) {
+	g := Guard{
+		Position:  Position{X: 0, Y: 0},
+		Direction: Direction{X: 0, Y: -1},
+	}
+	g.TurnRight()
+	assert.Equal(t, 1, g.Direction.X)
+	assert.Equal(t, 0, g.Direction.Y)
+	g.TurnRight()
+	assert.Equal(t, 0, g.Direction.X)
+	assert.Equal(t, 1, g.Direction.Y)
+	g.TurnRight()
+	assert.Equal(t, -1, g.Direction.X)
+	assert.Equal(t, 0, g.Direction.Y)
+	g.TurnRight()
+	assert.Equal(t, 0, g.Direction.X)
+	assert.Equal(t, -1, g.Direction.Y)
+}
+
+func TestMoveForward(t *testing.T) {
+	g := Guard{
+		Position:  Position{X: 0, Y: 0},
+		Direction: Direction{X: 0, Y: -1},
+	}
+	g.MoveForward()
+
+	assert.Equal(t, 0, g.Position.X)
+	assert.Equal(t, -1, g.Position.Y)
 }
